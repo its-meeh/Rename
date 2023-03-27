e18505aba0f980de9f27a1390ec6268140822da8
its-meeh <120617826+its-meeh@users.noreply.github.com>
2023-03-27 20:20:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -42,8 +42,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Owner 🇮🇳" ,url="https://t.me/zeuss145") ], 
+	[InlineKeyboardButton("Subscribe 🧐", url="https://t.me/botsproupdates") ]  ]))
 	    return
 	if id:
 	    if old == True:
@@ -55,8 +55,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Owner" ,url="https://t.me/zeuss145") ], 
+	[InlineKeyboardButton("Subscribe 🧐", url="https://t.me/botsproupdates") ]  ]))
 	        except:
 	             return
 	    else:
@@ -71,8 +71,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Owner 🇮🇳" ,url="https://t.me/zeuss145") ], 
+	[InlineKeyboardButton("Subscribe 🧐", url="https://t.me/botsproupdates") ]  ]))
 	         
 
 
